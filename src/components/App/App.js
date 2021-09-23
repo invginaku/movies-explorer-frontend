a9a4bcfb93ec3e88f48099b12ce685f6bbd750ef
Nikita Ivochkin <70746527+invginaku@users.noreply.github.com>
2021-09-23 18:58:36 +0300

    Components and file structure

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import { Switch, Route } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  return(
+      <Switch>
+        <Route />
+      </Switch>
+  );
+}
+
+export default App;
